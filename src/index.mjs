59b51bf1d0fd7d6b46d1c1350b4afdbbe1e4370b
Yan Lima Barbosa <yanbr763@gmail.com>
2024-07-25 05:07:59 -0300

    Get Requests

--- src/index.mjs
+++ src/index.mjs
@@ -6,6 +6,23 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "Hello" })
+})
+
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, username: "anson", displayName: "Anson" },
+    { id: 1, username: "john", displayName: "John" },
+    { id: 1, username: "richard", displayName: "Richard" },
+    { id: 1, username: "charles", displayName: "Charles" },
+  ])
+})
+
+app.get("/api/products", (request, response) => {
+  response.send([{ id: 123, name: "chichen breast", price: 12.99 }])
+})
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`)
 })
