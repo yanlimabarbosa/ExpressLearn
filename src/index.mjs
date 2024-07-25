425007e86484db5c25a897528296bf872b820094
Yan Lima Barbosa <yanbr763@gmail.com>
2024-07-25 05:21:32 -0300

    Route Parameters

--- src/index.mjs
+++ src/index.mjs
@@ -6,17 +6,35 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson" },
+  { id: 2, username: "john", displayName: "John" },
+  { id: 3, username: "richard", displayName: "Richard" },
+  { id: 4, username: "charles", displayName: "Charles" },
+]
+
 app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hello" })
 })
 
 app.get("/api/users", (request, response) => {
-  response.send([
-    { id: 1, username: "anson", displayName: "Anson" },
-    { id: 1, username: "john", displayName: "John" },
-    { id: 1, username: "richard", displayName: "Richard" },
-    { id: 1, username: "charles", displayName: "Charles" },
-  ])
+  response.send(mockUsers)
+})
+
+app.get("/api/users/:id", (request, response) => {
+  const userId = parseInt(request.params.id, 10)
+
+  if (isNaN(userId)) {
+    return response.status(400).json({ msg: "Bad Request. Invalid user ID." })
+  }
+
+  const user = mockUsers.find((user) => user.id === userId)
+
+  if (!user) {
+    return response.status(404).json({ msg: "User not found." })
+  }
+
+  return response.json(user)
 })
 
 app.get("/api/products", (request, response) => {
