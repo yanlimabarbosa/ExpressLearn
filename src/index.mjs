cd928522da2502c46f0b495c4dfa99d673a448c0
Yan Lima Barbosa <yanbr763@gmail.com>
2024-07-29 15:51:31 -0300

    Patch Requests

--- src/index.mjs
+++ src/index.mjs
@@ -70,11 +70,26 @@ app.put("/api/users/:id", (request, response) => {
 
   if (findUserIndex === -1) return response.sendStatus(400)
 
-  mockUsers[findUserIndex] = {
-    id: parsedId,
-    ...body,
-  }
+  mockUsers[findUserIndex] = { id: parsedId, ...body }
 
+  return response.sendStatus(200)
+})
+
+app.patch("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request
+
+  const parsedId = parseInt(id)
+
+  if (isNaN(parsedId)) return response.sendStatus(400)
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId)
+
+  if (findUserIndex === -1) return response.sendStatus(404)
+
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body }
   return response.sendStatus(200)
 })
 
